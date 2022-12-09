e1dec1de94c25ea9554d4c0e18b338a458b55d3b
Wokurexik <96776458+wokurexik@users.noreply.github.com>
2022-12-09 22:38:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+  var character = document.getElementById("character");
+  var block = document.getElementById("block");
+  var counter = 0;
+  function jump() {
+    if (character.classList == "animate") {
+      return;
+    }
+    character.classList.add("animate");
+    setTimeout(function () {
+      character.classList.remove("animate");
+    }, 300);
+  }
+  var checkDead = setInterval(function () {
+    let characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    let blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left")
+    );
+    if (blockLeft < 60 && blockLeft > -60 && characterTop >= 86) {
+      block.style.animation = "none";
+      alert("Game Over. score: " + Math.floor(counter / 100));
+      counter = 0;
+      block.style.animation = "block 1s infinite linear";
+    } else {
+      counter++;
+      document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+    }
+  }, 10);
+  
+  
+function startGame() {
+  var x = document.getElementById("block");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
